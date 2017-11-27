3d503b757707fa2a1cb15904d1de65223fdf417d
Werner Griesel <wernerg@dotslash.co.za>
2017-11-27 15:50:58 +0200

    Initial commit

---
+++ wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// Navigation ACTIVE class
+$(document).ready(function(){
+	$('.navbar-nav li a[href="' + document.location.pathname + '"]').parent('li').addClass('active');
+});
+
+// We put modals out of wrapper to working properly
+$(document).ready(function(){
+	$('.modal').appendTo("body");
+});
+
+// Tooltips and Popovers
+$(document).ready(function(){
+  $('[data-toggle="popover"]').popover();
+  $('[data-toggle="tooltip"]').tooltip();
+  $('[rel="tooltip"]').tooltip();
+})
+
